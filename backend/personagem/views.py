ac2796a8ddfff42539c3b2c1f2bb86580ce0c6b5
Vitor <vitor.gan21@gmail.com>
2020-03-14 11:37:18 -0300

    Alterações menores do backend

--- backend/personagem/views.py
+++ backend/personagem/views.py
@@ -4,36 +4,20 @@ from django.contrib.auth.models import User, AnonymousUser
 
 
 def get_personagens(request):
-    personagens = [
-            {
-                'nome': 'Personagem 1',
-                'nivel': 2,
-                'classes': [
-                    'lvl xx',
-                    'lvl xy'
-                ] 
-            },
-            {
-                'nome': 'Personagem 2',
-                'nivel': 3,
-                'classes': [
-                    'lvl xx',
-                    'lvl xy',
-                    'lvl yy'
-                ] 
-            }
-    ]
 
     qtd = User.objects.count()
     user = User.objects.first()
     print(f'User {request.user}')
 
     if(type(request.user) != AnonymousUser):
-        #personagens = Campanha.objects.filter(mestre=user)
-        if personagens:
-            personagens = personagens
-        else:
+        personagens = Personagem.objects.filter(jogador=user)
+        personagens = list(personagens.values())
+        
+        if not personagens:
             personagens = 'Vazio'
+    
     else:
         personagens = 'you must be logged in to see your campaigns'
-    return JsonResponse({"personagens": personagens})
+
+    data = personagens
+    return JsonResponse(data, safe=False)
