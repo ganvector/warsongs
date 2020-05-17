a346b688c2faf324aa6de6317d25ef087a330203
Vitor <vitor.gan21@gmail.com>
2020-05-17 00:55:33 -0300

    Package ESLint
    Adicionado pacote do ESLint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js']
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'warn',
+    "react/destructuring-assignment": 'off',
+    "react/jsx-one-expression-per-line": 'off',
+    "object-curly-newline": 'off'
+  },
+};
