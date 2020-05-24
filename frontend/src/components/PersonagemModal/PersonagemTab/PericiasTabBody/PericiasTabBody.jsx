7b5e0ffa35962f0f51ce71aaaf20c6ccd5b13d9c
Vitor <vitor.gan21@gmail.com>
2020-05-24 17:08:03 -0300

    Link para pericias e para os sentidos

--- frontend/src/components/PersonagemModal/PersonagemTab/PericiasTabBody/PericiasTabBody.jsx
+++ frontend/src/components/PersonagemModal/PersonagemTab/PericiasTabBody/PericiasTabBody.jsx
@@ -3,17 +3,19 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import TesteResistencias from './TesteResistencias/TesteResistencias';
+import Pericias from './Pericias/Pericias';
+import Sentidos from './Sentidos/Sentidos';
 
 const periciasTabBody = (props) => (
   <Row>
     <Col>
       <TesteResistencias teste_res={props.teste_res} />
-      <Row>
-        <Col>Pericias</Col>
-      </Row>
-      <Row>
-        <Col>Sentidos</Col>
-      </Row>
+      <Pericias pericias={props.pericias} />
+      <Sentidos
+        percepcao={props.pericias.percepcao}
+        investigacao={props.pericias.investigacao}
+        intuicao={props.pericias.intuicao}
+      />
     </Col>
   </Row>
 );
