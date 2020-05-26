7109283077a4092e216c476f913ca0eafa5ddf16
Vitor <vitor.gan21@gmail.com>
2020-05-25 22:34:09 -0300

    Ordem alfabetica
    Colocadas pericias em ordem alfabetica e adicionado span para representar o atributo

--- frontend/src/components/PersonagemModal/PersonagemTab/PericiasTabBody/Pericias/Pericias.jsx
+++ frontend/src/components/PersonagemModal/PersonagemTab/PericiasTabBody/Pericias/Pericias.jsx
@@ -29,14 +29,18 @@ const pericias = (props) => (
               <Row>
                 {proficiente(props.pericias.acrobacia.proficiente)}
                 <Col>{formatarValor(props.pericias.acrobacia.valor)}</Col>
-                <Col>Acrobacia</Col>
+                <Col>
+                  Acrobacia <span>(Des)</span>
+                </Col>
               </Row>
             </Col>
             <Col>
               <Row>
-                {proficiente(props.pericias.investigacao.proficiente)}
-                <Col>{formatarValor(props.pericias.investigacao.valor)}</Col>
-                <Col>Investigacão</Col>
+                {proficiente(props.pericias.intuicao.proficiente)}
+                <Col>{formatarValor(props.pericias.intuicao.valor)}</Col>
+                <Col>
+                  Intuição <span>(Sab)</span>
+                </Col>
               </Row>
             </Col>
           </Row>
@@ -45,14 +49,18 @@ const pericias = (props) => (
               <Row>
                 {proficiente(props.pericias.adestrarAnimais.proficiente)}
                 <Col>{formatarValor(props.pericias.adestrarAnimais.valor)}</Col>
-                <Col>Adestrar Animais</Col>
+                <Col>
+                  Adestrar Animais <span>(Sab)</span>
+                </Col>
               </Row>
             </Col>
             <Col>
               <Row>
-                {proficiente(props.pericias.medicina.proficiente)}
-                <Col>{formatarValor(props.pericias.medicina.valor)}</Col>
-                <Col>Medicina</Col>
+                {proficiente(props.pericias.investigacao.proficiente)}
+                <Col>{formatarValor(props.pericias.investigacao.valor)}</Col>
+                <Col>
+                  Investigação <span>(Int)</span>
+                </Col>
               </Row>
             </Col>
           </Row>
@@ -61,14 +69,18 @@ const pericias = (props) => (
               <Row>
                 {proficiente(props.pericias.arcanismo.proficiente)}
                 <Col>{formatarValor(props.pericias.arcanismo.valor)}</Col>
-                <Col>Arcanismo</Col>
+                <Col>
+                  Arcanismo <span>(Int)</span>
+                </Col>
               </Row>
             </Col>
             <Col>
               <Row>
-                {proficiente(props.pericias.natureza.proficiente)}
-                <Col>{formatarValor(props.pericias.natureza.valor)}</Col>
-                <Col>Natureza</Col>
+                {proficiente(props.pericias.medicina.proficiente)}
+                <Col>{formatarValor(props.pericias.medicina.valor)}</Col>
+                <Col>
+                  Medicina <span>(Sab)</span>
+                </Col>
               </Row>
             </Col>
           </Row>
@@ -77,14 +89,38 @@ const pericias = (props) => (
               <Row>
                 {proficiente(props.pericias.atletismo.proficiente)}
                 <Col>{formatarValor(props.pericias.atletismo.valor)}</Col>
-                <Col>Atletismo</Col>
+                <Col>
+                  Atletismo <span>(For)</span>
+                </Col>
+              </Row>
+            </Col>
+            <Col>
+              <Row>
+                {proficiente(props.pericias.natureza.proficiente)}
+                <Col>{formatarValor(props.pericias.natureza.valor)}</Col>
+                <Col>
+                  Natureza <span>(Int)</span>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Row>
+                {proficiente(props.pericias.atuacao.proficiente)}
+                <Col>{formatarValor(props.pericias.atuacao.valor)}</Col>
+                <Col>
+                  Atuação <span>(Car)</span>
+                </Col>
               </Row>
             </Col>
             <Col>
               <Row>
                 {proficiente(props.pericias.percepcao.proficiente)}
                 <Col>{formatarValor(props.pericias.percepcao.valor)}</Col>
-                <Col>Percepção</Col>
+                <Col>
+                  Percepção <span>(Sab)</span>
+                </Col>
               </Row>
             </Col>
           </Row>
@@ -93,14 +129,18 @@ const pericias = (props) => (
               <Row>
                 {proficiente(props.pericias.blefar.proficiente)}
                 <Col>{formatarValor(props.pericias.blefar.valor)}</Col>
-                <Col>Blefar</Col>
+                <Col>
+                  Blefar <span>(Car)</span>
+                </Col>
               </Row>
             </Col>
             <Col>
               <Row>
-                {proficiente(props.pericias.atuacao.proficiente)}
-                <Col>{formatarValor(props.pericias.atuacao.valor)}</Col>
-                <Col>Atuação</Col>
+                {proficiente(props.pericias.persuasao.proficiente)}
+                <Col>{formatarValor(props.pericias.persuasao.valor)}</Col>
+                <Col>
+                  Persuasão <span>(Car)</span>
+                </Col>
               </Row>
             </Col>
           </Row>
@@ -109,14 +149,18 @@ const pericias = (props) => (
               <Row>
                 {proficiente(props.pericias.furtividade.proficiente)}
                 <Col>{formatarValor(props.pericias.furtividade.valor)}</Col>
-                <Col>Furtividade</Col>
+                <Col>
+                  Furtividade <span>(Des)</span>
+                </Col>
               </Row>
             </Col>
             <Col>
               <Row>
-                {proficiente(props.pericias.persuasao.proficiente)}
-                <Col>{formatarValor(props.pericias.persuasao.valor)}</Col>
-                <Col>Persuasão</Col>
+                {proficiente(props.pericias.prestidigitacao.proficiente)}
+                <Col>{formatarValor(props.pericias.prestidigitacao.valor)}</Col>
+                <Col>
+                  Prestidigitação <span>(Des)</span>
+                </Col>
               </Row>
             </Col>
           </Row>
@@ -125,14 +169,18 @@ const pericias = (props) => (
               <Row>
                 {proficiente(props.pericias.historia.proficiente)}
                 <Col>{formatarValor(props.pericias.historia.valor)}</Col>
-                <Col>História</Col>
+                <Col>
+                  História <span>(Int)</span>
+                </Col>
               </Row>
             </Col>
             <Col>
               <Row>
-                {proficiente(props.pericias.prestidigitacao.proficiente)}
-                <Col>{formatarValor(props.pericias.prestidigitacao.valor)}</Col>
-                <Col>Prestidigitacao</Col>
+                {proficiente(props.pericias.religiao.proficiente)}
+                <Col>{formatarValor(props.pericias.religiao.valor)}</Col>
+                <Col>
+                  Religião <span>(Int)</span>
+                </Col>
               </Row>
             </Col>
           </Row>
@@ -141,30 +189,18 @@ const pericias = (props) => (
               <Row>
                 {proficiente(props.pericias.intimidacao.proficiente)}
                 <Col>{formatarValor(props.pericias.intimidacao.valor)}</Col>
-                <Col>Intimidação</Col>
-              </Row>
-            </Col>
-            <Col>
-              <Row>
-                {proficiente(props.pericias.religiao.proficiente)}
-                <Col>{formatarValor(props.pericias.religiao.valor)}</Col>
-                <Col>Religião</Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Row>
-                {proficiente(props.pericias.intuicao.proficiente)}
-                <Col>{formatarValor(props.pericias.intuicao.valor)}</Col>
-                <Col>Intuição</Col>
+                <Col>
+                  Intimidação <span>(Car)</span>
+                </Col>
               </Row>
             </Col>
             <Col>
               <Row>
                 {proficiente(props.pericias.sobrevivencia.proficiente)}
                 <Col>{formatarValor(props.pericias.sobrevivencia.valor)}</Col>
-                <Col>Sobrevivência</Col>
+                <Col>
+                  Sobrevivência <span>(Sab)</span>
+                </Col>
               </Row>
             </Col>
           </Row>
