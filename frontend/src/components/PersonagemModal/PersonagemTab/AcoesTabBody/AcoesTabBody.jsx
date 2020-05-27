367a2b23b7123306c97b847fc9f68224c4e401b6
Vitor <vitor.gan21@gmail.com>
2020-05-26 23:50:06 -0300

    usando tabs para descrever acoes

--- frontend/src/components/PersonagemModal/PersonagemTab/AcoesTabBody/AcoesTabBody.jsx
+++ frontend/src/components/PersonagemModal/PersonagemTab/AcoesTabBody/AcoesTabBody.jsx
@@ -1,45 +1,60 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
 import AcoesAtaques from './AcoesAtaques/AcoesAtaques';
 
-const acoesTabBody = (props) => {
-  return (
-    <Row>
-      <Col>
-        <Row>
-          <Col>
-            <Row>
-              <Col>
-                <strong>Ações em combate</strong>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                Atacar, lançar magia, correr, desengajar, fugir, segurar,
-                ajudar, esconder, improvisar, preparar, procurar, empurrar, usar
-                um objeto.
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <Col>
-                <strong>Ataques e Magias</strong>
-              </Col>
-            </Row>
-            <AcoesAtaques />
-            <Row>
-              <Col>Magias</Col>
-            </Row>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  );
-};
+const acoesTabBody = (props) => (
+  <Row>
+    <Col>
+      <Row>
+        <Col>
+          <Row>
+            <Col>
+              <Tabs
+                variant="pills"
+                defaultActiveKey="acoes"
+                id="tab_acoes_personagem"
+              >
+                <Tab eventKey="acoes" title="Ações">
+                  <Row>
+                    <Col>
+                      <strong>Ações em combate</strong>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      Atacar, lançar magia, correr, desengajar, fugir, segurar,
+                      ajudar, esconder, improvisar, preparar, procurar,
+                      empurrar, usar um objeto.
+                    </Col>
+                  </Row>
+                </Tab>
+                <Tab eventKey="acao_bonus" title="Ação Bônus">
+                  Ação Bonus
+                </Tab>
+                <Tab eventKey="reacao" title="Reação">
+                  <Row>
+                    <Col>
+                      <strong>Reações em combate</strong>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>Ataques de oportunidade.</Col>
+                  </Row>
+                </Tab>
+                <Tab eventKey="outros" title="Outros">
+                  outros
+                </Tab>
+              </Tabs>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Col>
+  </Row>
+);
 
 export default acoesTabBody;
