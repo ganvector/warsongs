d3471d5aa2e47baa2a613ac5a495b2dbad070b5a
Vitor Nascimento <vitor.gan21@gmail.com>
2020-07-04 16:58:19 -0300

    Adicionando arquivo para componentes futuros

---
+++ frontend/src/components/PersonagemModal/PersonagemTab/CaracteristicasTabBody/RacialCaracteristicas/RacialCaracteristicas.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+//listarCaracteristicas
+
+const racialCaracteristicas = (props) => {
+  return (
+    <Row>
+      <Col>
+        <Row>
+          <Col className="SegmentTitle">Caracteristicas de Raça</Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default racialCaracteristicas;
---
+++ frontend/src/components/PersonagemModal/PersonagemTab/CaracteristicasTabBody/TalentosCaracteristicas/TalentosCaracteristicas.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+//listarCaracteristicas
+
+const talentosCaracteristicas = (props) => {
+  return (
+    <Row>
+      <Col>
+        <Row>
+          <Col className="SegmentTitle">Caracteristicas de Talentos</Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default talentosCaracteristicas;
