add82a752b13082bffa8e0ad610ef9305888a566
Vitor Nascimento <vitor.gan21@gmail.com>
2020-07-05 10:40:57 -0300

    Adicionado caracteristicas de raca

--- frontend/src/components/PersonagemModal/PersonagemTab/CaracteristicasTabBody/RacialCaracteristicas/RacialCaracteristicas.jsx
+++ frontend/src/components/PersonagemModal/PersonagemTab/CaracteristicasTabBody/RacialCaracteristicas/RacialCaracteristicas.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import DescricaoDaCaracteristica from '../caracteristicasDescricao';
 
 //listarCaracteristicas
 
 const racialCaracteristicas = (props) => {
+  let caracteristicas = props.raca.caracteristicas.map(caracteristica => {
+    return <DescricaoDaCaracteristica caracteristica={caracteristica}/>;
+  });
   return (
-    <Row>
+    <Row className={props.className}>
       <Col>
-        <Row>
-          <Col className="SegmentTitle">Caracteristicas de Raça</Col>
+        <Row id={`caracteristica${props.raca.nome}`}>
+          <Col>
+            <Row>
+              <Col className={"CaracteristicaTitulo"}>Raciais</Col>
+            </Row>
+            <Row className="CaractesticaSubtitulo">
+              <Col>
+                { props.raca.nome }
+              </Col>
+            </Row>
+            { caracteristicas }
+          </Col>
         </Row>
       </Col>
     </Row>
