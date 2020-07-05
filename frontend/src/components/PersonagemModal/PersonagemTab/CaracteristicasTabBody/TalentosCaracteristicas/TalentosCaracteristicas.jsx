9ee147b91b748cf3c0e3da20dc98e65b033b2e4f
Vitor Nascimento <vitor.gan21@gmail.com>
2020-07-05 11:16:50 -0300

    Adicionado tab de talentos

--- frontend/src/components/PersonagemModal/PersonagemTab/CaracteristicasTabBody/TalentosCaracteristicas/TalentosCaracteristicas.jsx
+++ frontend/src/components/PersonagemModal/PersonagemTab/CaracteristicasTabBody/TalentosCaracteristicas/TalentosCaracteristicas.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import DescricaoDaCaracteristica from '../caracteristicasDescricao';
 
 //listarCaracteristicas
 
 const talentosCaracteristicas = (props) => {
+  let caracteristicas = props.talentos.map(caracteristica => {
+    return <DescricaoDaCaracteristica caracteristica={caracteristica}/>;
+  });
   return (
-    <Row>
+    <Row className={props.className}>
       <Col>
-        <Row>
-          <Col className="SegmentTitle">Caracteristicas de Talentos</Col>
+        <Row id={`caracteristicaTalentos`}>
+          <Col>
+            <Row>
+              <Col className={"CaracteristicaTitulo"}>Talentos</Col>
+            </Row>
+            { caracteristicas }
+          </Col>
         </Row>
       </Col>
     </Row>
