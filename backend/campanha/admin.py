9b62cc74d481e840eab75ea0e9f8f1bb3d4e9aff
Vitor Nascimento <vitor.gan21@gmail.com>
2020-08-09 20:04:06 -0300

    Atualiznado admin

--- backend/campanha/admin.py
+++ backend/campanha/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models.campanha import Campanha
 from .models.raca import Raca
-from .models.classe import Classe
+from .models.classe import Classe, CaracteristicasClasse
+from .models.aventura import Aventura
+from .models.sessao import Sessao
 
 
 admin.site.register(Campanha)
+admin.site.register(Aventura)
+admin.site.register(Sessao)
 admin.site.register(Raca)
 admin.site.register(Classe)
+admin.site.register(CaracteristicasClasse)
