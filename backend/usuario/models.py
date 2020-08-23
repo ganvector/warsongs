511a09836b65de31990eafd3c396d84530408c1f
Vitor Nascimento <vitor.gan21@gmail.com>
2020-08-23 19:09:28 -0300

    Adicionando novo campo

--- backend/usuario/models.py
+++ backend/usuario/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100, default='Perfil')
+    online = models.BooleanField(default=False)
 
     def __str__(self):
         return self.usuario.username
